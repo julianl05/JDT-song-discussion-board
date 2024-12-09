30c9abaf663f32c555b7f20e73fe4ecd2785320c
Julian Lim Jun Ren <limjunre@bu.edu>
2024-12-09 01:00:05 -0500

    small change to currentDate.jsx

--- frontend/src/components/currentDate/currentDate.jsx
+++ frontend/src/components/currentDate/currentDate.jsx
@@ -6,16 +6,13 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 function currentDate() {
     
     const currentDate = new Date();
-    const day = currentDate.getDate().toString().padStart(2, '0'); 
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); 
-    const year = currentDate.getFullYear();
 
     const arrowStyles = {
       fontSize: 50,
       cursor: 'pointer',
       color: '#333333',
       '&:hover': {
-          color: 'gray'  // Color changes to blue on hover
+          color: 'gray'  
       }
     }
 
