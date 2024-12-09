2c42f419b254a545e7a77d0d240ad8d8377638a2
Julian Lim <limjunre@bu.edu>
2024-12-09 03:16:34 -0500

    GENIUS API FETCH NOW WORKS. Random songs implemented.

--- backend/index.js
+++ backend/index.js
@@ -24,10 +24,17 @@ app.get('/songs/:id', async (req, res) => {
             }
         });
         console.log(response.data); 
+        // console.log(response.status);
         res.json(response.data.response.song); 
     } 
     catch (error) {
         console.error('Error fetching song data:', error);
+        console.log('STATUS CODE OF ERROR: ', error.response.status);
+        res.status(error.response.status).send({
+            message: error.message,
+            details: error.response.data
+        });
+        console.log(error.response.status);
     }
 });
 
--- frontend/src/components/songs/songs.jsx
+++ frontend/src/components/songs/songs.jsx
@@ -5,6 +5,15 @@ import cover2 from '../../assets/hippocampus.jpg'
 import cover3 from '../../assets/halfalive.jpg'
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 function Songs() {
+    const [loading, setLoading] = useState(true);
+    const [songData, setSongData] = useState([]);
+    const maxSong= 2471960;
+    const minSong= 1;
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
     const songs = [
         { title: "Song 1", artist: "Artist 1", cover: cover1 },
         { title: "Song 2", artist: "Artist 2", cover: cover2 },
@@ -13,31 +22,61 @@ function Songs() {
 
     useEffect(() => {
         console.log('Fetching song data...');
-        fetch('http://localhost:4000/songs/378195') 
-            .then(response => response.json())
-            .then(data => {
-                console.log('Song data:', data);
-            })
-            .catch(error => {
-                console.error('Error fetching song data:', error);
-            });
+        const fetchSong = async () => {
+            const validSongs = [];
+            while (validSongs.length < 3) {
+                const randSong = getRandomInt(minSong, maxSong);
+                try {
+                    const response = await fetch(`http://localhost:4000/songs/${randSong}`)
+                    if (!response.ok) {
+                        console.error('Failed to fetch song data', response);
+                        continue;
+                    }
+                    if (response.status === 404) {   
+                        console.log('Invalid song data', data);
+                        continue;
+                    }
+                    console.log('Song data fetched successfully');
+                    const data = await response.json();
+                    
+                    const song = {
+                        title: data['full_title'],
+                        artist: data['artist_names'],
+                        cover: data['song_art_image_url']
+                    }
+                    
+                    validSongs.push(song);
+                } catch (error) {
+                    console.error("error fetching song data", error);
+                }
+                
+            }
+            setSongData(validSongs);
+            setLoading(false);
+        };
+        fetchSong();
+        
     }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <div id="songs">
-            {songs.map((song, index) => (
+            {songData.map((songData, index) => (
                 <Card key={index} class="card">
                     <CardMedia
-                        sx={{ height: 200 }}
-                        image={song.cover}
-                        title={`Cover for ${song.title}`}
+                        sx={{ height: 300 }}
+                        image={songData.cover}
+                        title={`Cover for ${songData.title}`}
                     />
-                    <CardContent>
-                        <Typography variant="h5" component="h2">
-                            {song.title}
+                    <CardContent class="CardContent">
+                        <Typography variant="h6" component="h2">
+                            {songData.title}
                         </Typography>
                         <Typography color="lightgray">
-                            {song.artist}
+                            {songData.artist}
                         </Typography>
                     </CardContent>
                 </Card>
